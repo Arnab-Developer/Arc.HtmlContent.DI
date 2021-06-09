8c947995cdeeca20409f180e811a35dfa3d9b8f5
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-09 21:39:00 +0530

    Write XML comments (#1)

--- Arc.HtmlContent.DI/ServiceCollectionExtensions.cs
+++ Arc.HtmlContent.DI/ServiceCollectionExtensions.cs
@@ -3,8 +3,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ArnabDeveloper.HttpHealthCheck.DI
 {
+    /// <summary>
+    /// Service collection extensions
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add html content service
+        /// </summary>
+        /// <param name="services">IServiceCollection type object</param>
+        /// <returns>IServiceCollection type object</returns>
         public static IServiceCollection AddHtmlContentService(this IServiceCollection services) =>
             services.AddTransient(typeof(IHtmlContentService), typeof(HtmlContentService));
     }
